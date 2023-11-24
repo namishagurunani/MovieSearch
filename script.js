3a4ed96c28cc7bab0cfbda60e3d72c9f53ddca3b
Namisha Gurunani <126158413+namishagurunani@users.noreply.github.com>
2023-11-25 01:26:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,182 +1,65 @@
-//  for API integration and debouncing
- const API_KEY = '1ca6ed19';
- const searchInput = document.getElementById('searchInput');
- const movieList = document.getElementById('movieList');
- const detailsPage = document.getElementById('detailsPage');
- let debounceTimeout;
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
- // Function to fetch movie data from the OMDB API
- async function fetchMovies(query) {
-     try {
-         const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`);
-         const data = await response.json();
-         return data.Search || [];
-     } catch (error) {
-         console.error('API Error:', error);
-         return [];
-     }
- }
- 
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
- // Function to display movie search results
-function displayMovies(movies) {
-    movieList.innerHTML = '';
-    if (movies.length === 0) {
-        movieList.innerHTML = 'No results found.';
-        return;
-    }
-    movies.forEach((movie) => {
-        const movieCard = document.createElement('div');
-        movieCard.className = 'movie-card';
+let debounceTimer;
 
-        // Check if the poster is "N/A" and display a default image or message
-        if (movie.Poster !== "N/A") {
-            movieCard.innerHTML = `
-                <img src="${movie.Poster}" alt="${movie.Title}">
-                <h2>${movie.Title}</h2>
-                <p>${movie.Year}</p>
-                <button data-id="${movie.imdbID}">More Info</button>
-            `;
-        } else {
-            movieCard.innerHTML = `
-                <div class="no-poster">
-                    <p>No poster available</p>
-                </div>
-            `;
-        }
-        movieList.appendChild(movieCard);
-    });
-}
-// Function to fetch detailed movie data by IMDb ID
-async function fetchMovieDetails(imdbID) {
-    try {
-        const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&i=${imdbID}`);
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('API Error:', error);
-        return null;
-    }
+async function getMovies(url) {
+  const resp = await fetch(url);
+  const respData = await resp.json();
+  showMovies(respData.results);
 }
 
- // Function to fetch movie data from the OMDB API
-        async function fetchMovies(query) {
-            try {
-                const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`);
-                const data = await response.json();
-                return data.Search || [];
-            } catch (error) {
-                console.error('API Error:', error);
-                return [];
-            }
-        }
+function showMovies(movies) {
+  main.innerHTML = "";
+  movies.forEach((movie) => {
+    const { poster_path, title, vote_average, overview } = movie;
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
 
-        // Function to fetch detailed movie data by IMDb ID
-        async function fetchMovieDetails(imdbID) {
-            try {
-                const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&i=${imdbID}`);
-                const data = await response.json();
-                return data;
-            } catch (error) {
-                console.error('API Error:', error);
-                return null;
-            }
-        }
+    movieEl.innerHTML = `
+      <img src="${IMGPATH + poster_path}" alt="${title}"/>
+      <div class="movie-info">
+        <h3>${title}</h3>
+        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+      </div>
+      <div class="overview">
+        <h2>Overview:</h2>
+        ${overview}
+      </div>
+    `;
 
-        // Function to display movie details on the movie card
-        function displayMovieDetailsOnCard(card, movie) {
-            card.innerHTML = `
-                <img src="${movie.Poster}" alt="${movie.Title}">
-                <h2>${movie.Title}</h2>
-                <p>${movie.Year}</p>
-                <p>${movie.Plot}</p>
-                <p>Director: ${movie.Director}</p>
-                <p>Cast: ${movie.Actors}</p>
-                <p>Released: ${movie.Released}</p>
-                <p>Ratings: ${movie.imdbRating}</p>
-                <button onclick="hideDetails(this)">Close</button>
-            `;
-        }
+    main.appendChild(movieEl);
+  });
+}
 
-        // Function to hide movie details on the movie card
-        function hideDetails(button) {
-            const card = button.parentElement;
-            card.innerHTML = ''; // Remove the details
-        }
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return 'green';
+  } else if (vote >= 5) {
+    return 'orange';
+  } else {
+    return 'red';
+  }
+}
 
-        // Event listener for the search input
-        searchInput.addEventListener('input', async () => {
-            const query = searchInput.value;
-            const movies = await fetchMovies(query);
-            displayMovies(movies);
-        });
+function debounce(func, delay) {
+  clearTimeout(debounceTimer);
+  debounceTimer = setTimeout(func, delay);
+}
 
-        // Function to display movie cards
-        function displayMovies(movies) {
-            movieList.innerHTML = '';
-            if (movies.length === 0) {
-                movieList.innerHTML = 'No results found.';
-                return;
-            }
-            movies.forEach((movie) => {
-                const movieCard = document.createElement('div');
-                movieCard.className = 'movie-card';
-
-                // Check if the poster is "N/A" and display a default image or message
-                if (movie.Poster !== "N/A") {
-                    movieCard.innerHTML = `
-                        <img src="${movie.Poster}" alt="${movie.Title}">
-                        <h2>${movie.Title}</h2>
-                        <p>${movie.Year}</p>
-                        <button data-id="${movie.imdbID}" onclick="showDetails(this)">More Info</button>
-                    `;
-                } else {
-                    movieCard.innerHTML = `
-                        <div class="no-poster">
-                            <p>No poster available</p>
-                        </div>
-                    `;
-                }
-                movieList.appendChild(movieCard);
-            });
-        }
-
-        // Function to show movie details on the movie card when the "More Info" button is clicked
-        function showDetails(button) {
-            const imdbID = button.getAttribute('data-id');
-            fetchMovieDetails(imdbID)
-                .then((movie) => {
-                    if (movie) {
-                        displayMovieDetailsOnCard(button.parentElement, movie);
-                    }
-                });
-        }
-
- // Function to handle debouncing and search
- function handleDebouncedSearch() {
-     const query = searchInput.value;
-     if (debounceTimeout) {
-         clearTimeout(debounceTimeout);
-     }
-     debounceTimeout = setTimeout(async () => {
-         const movies = await fetchMovies(query);
-         displayMovies(movies);
-     }, 300); // Adjust the debounce delay as needed
- }
-
- // Event listeners
- searchInput.addEventListener('input', handleDebouncedSearch);
- movieList.addEventListener('click', (e) => {
-     if (e.target.tagName === 'BUTTON') {
-         const imdbID = e.target.getAttribute('data-id');
-         // Add code to display movie details, including plot summaries, cast, release dates, and ratings.
-     }
- });
- 
-//  for theme switching
-        const themeSwitcher = document.getElementById('theme-switcher');
-        const body = document.body;
-
-        themeSwitcher.addEventListener('click', () => {
-            body.classList.toggle('dark-mode');
-        });
+search.addEventListener("input", () => {
+  debounce(() => {
+    const searchTerm = search.value;
+    if (searchTerm) {
+      getMovies(SEARCHAPI + searchTerm);
+    }
+  }, 300); // Adjust the delay (in milliseconds) according to your needs
+});
+document.addEventListener("DOMContentLoaded", () => {
+    getMovies(APIURL);
+  });
